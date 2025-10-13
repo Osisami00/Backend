f58cd05723dcee74577d21db0778f5821e505b4d
Osisami <osisamimichael1@gmail.com>
2025-10-13 15:58:25 +0100

    Backend Course

---
+++ main.py
@@ -0,0 +1,6 @@
+import my_module
+
+# calling funcs from my_module
+
+print(my_module.greet("Michael"))
+print(my_module.sum(10,15))
---
+++ my_module.py
@@ -0,0 +1,7 @@
+def greet(name):
+    """This function greets the user"""
+    return f"Hello, {name}!"
+
+def sum(a,b):
+    """This function adds two numbers"""
+    return a + b
